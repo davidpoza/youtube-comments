f5f24f822987d89af36c199b77bf087397d8acb4
David Poza Suárez <pozasuarez@gmail.com>
2020-05-10 17:47:17 +0200

    modified videoUrlIsValid helper

--- src/components/helpers/utils.js
+++ src/components/helpers/utils.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export function videoUrlIsValid(url) {
-  const regex = /(?:https:\/\/(?:www\.)?)?youtube.com\/watch\?v=([A-Za-z0-9]*)/;
+  const regex = /(?:https:\/\/(?:www\.)?)?youtube.com\/watch\?v=([A-Za-z0-9-_]*)/;
   const result = url.match(regex);
   return (result ? result[1] : false);
 }
