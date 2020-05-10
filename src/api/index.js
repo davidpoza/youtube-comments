811d80c245ca7ddfa2168c234b0f2a45766cf565
David Poza Suárez <pozasuarez@gmail.com>
2020-05-10 18:34:13 +0200

    added mock for videos api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import fetch from 'isomorphic-fetch';
 import config from '../config';
-import mockGetComments from './mocks/get-comments';
+import mockSearchComments from './mocks/search-comments';
+import mockListVideos from './mocks/list-videos';
 
 export default {
   user: {
@@ -16,7 +17,17 @@ export default {
         '&maxResults=100',
         `&searchTerms=${keywords}`, // TODO: encode
       ].join('');
-      return (Promise.resolve(mockGetComments));
+      return (Promise.resolve(mockSearchComments));
+    },
+  },
+  videos: {
+    list(videoId) {
+      const q = [
+        config.apiBaseUrl,
+        'part=snippet',
+        `&id=${videoId}`,
+      ].join('');
+      return (Promise.resolve(mockListVideos));
     },
   },
 };
---
+++ src/api/mocks/list-videos.js
@@ -0,0 +1,64 @@
+/* eslint-disable max-len */
+export default {
+  kind: 'youtube#videoListResponse',
+  etag: 'bq4PYPfsOtWLgABg1znFMyer8c0',
+  items: [
+    {
+      kind: 'youtube#video',
+      etag: 'tKv108SzKALJmvlWbeaDSVOBZco',
+      id: '52P1unHv7ME',
+      snippet: {
+        publishedAt: '2020-04-28T02:27:29Z',
+        channelId: 'UCfgOUydlU2iIrKjWQKmoB5A',
+        title: 'RESISTENCIA SMD LAPTOP QUE TANTO FALLAN?',
+        description:
+          'Como realizar  mediciones  basicas de corriente DC ,en componentes SMD como las resistencias-consideraciones\n\n####SUSCRIBETE#####\n\n\nGRACIAS POR VER MIS VIDEOS',
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/52P1unHv7ME/default.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/52P1unHv7ME/mqdefault.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/52P1unHv7ME/hqdefault.jpg',
+            width: 480,
+            height: 360,
+          },
+          standard: {
+            url: 'https://i.ytimg.com/vi/52P1unHv7ME/sddefault.jpg',
+            width: 640,
+            height: 480,
+          },
+        },
+        channelTitle: 'ELECTROSOTOX',
+        tags: [
+          'CURSO REPARACION LAPTOP',
+          'MEDICION RESISTOR SMD',
+          'COMO REPARAR LAPTOP',
+          'electrosotox',
+          'curso de electronica gratis',
+        ],
+        categoryId: '28',
+        liveBroadcastContent: 'none',
+        defaultLanguage: 'es-419',
+        localized: {
+          title: 'RESISTENCIA SMD LAPTOP QUE TANTO FALLAN?',
+          description:
+            'Como realizar  mediciones  basicas de corriente DC ,en componentes SMD como las resistencias-consideraciones\n\n####SUSCRIBETE#####\n\n\nGRACIAS POR VER MIS VIDEOS',
+        },
+        defaultAudioLanguage: 'es-419',
+      },
+    },
+  ],
+  pageInfo: {
+    totalResults: 1,
+    resultsPerPage: 1,
+  },
+};
+
+/* eslint-enable max-len */
