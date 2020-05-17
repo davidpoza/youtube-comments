03cd468204e0488abc09ddcc08fef183fa42f91a
David Poza Suárez <pozasuarez@gmail.com>
2020-05-17 12:58:34 +0200

    replies sorting by date desc

--- src/components/replies/index.jsx
+++ src/components/replies/index.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
+import moment from 'moment';
 import Comment from '../comment';
 
 export default function Replies(props) {
   const { replies } = props;
   if (replies) {
     return (
-      replies.map((r) => (
-        <Comment
-          isReply
-          text={r.text}
-          authorImage={r.authorImage}
-          authorName={r.authorName}
-          authorUrl={r.authorUrl}
-          publishedDate={r.publishedDate}
-        />
-      ))
+      replies
+        .sort((a, b) => {
+          const dateA = moment(a.publishedDate);
+          const dateB = moment(b.publishedDate);
+          if (dateA.isBefore(dateB)) {
+            return (-1);
+          }
+          if (dateB.isBefore(dateA)) {
+            return (1);
+          }
+          return (0);
+        })
+        .map((r) => (
+          <Comment
+            isReply
+            text={r.text}
+            authorImage={r.authorImage}
+            authorName={r.authorName}
+            authorUrl={r.authorUrl}
+            publishedDate={r.publishedDate}
+          />
+        ))
     );
   }
   return (null);
