4efedb93e8fc89e2508874e9f707cc131120abe4
David Poza Suárez <pozasuarez@gmail.com>
2020-05-23 16:44:12 +0200

    fix pagination margin on mobile

--- src/components/pagination/useStyles.js
+++ src/components/pagination/useStyles.js
@@ -1,9 +1,15 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   root: {
     display: 'flex',
     justifyContent: 'center',
-    margin: '2em',
+    [theme.breakpoints.up('md')]: {
+      margin: '2em',
+    },
+    [theme.breakpoints.down('sm')]: {
+      margin: '0.5em',
+      marginTop: '2em',
+    },
   },
 }));
