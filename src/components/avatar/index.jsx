3ab5ef6a56166d9d8001b79db94fd5d85b9f7f27
David Poza Suárez <pozasuarez@gmail.com>
2020-06-01 21:14:11 +0200

    added avatar menu with logout option

--- src/components/avatar/index.jsx
+++ src/components/avatar/index.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import Avatar from '@material-ui/core/Avatar';
+import useStyles from './useStyles';
 
 export default function MyAvatar(props) {
+  const classes = useStyles();
   const { userId } = props;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   if (userId) {
     return (
-      <Avatar className="avatar" src={`${process.env.REACT_APP_API_URL}/users/avatar/${userId}`} />
+      <>
+        <Avatar
+          onClick={handleClick}
+          className={classes.avatar}
+          src={`${process.env.REACT_APP_API_URL}/users/avatar/${userId}`}
+        />
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+      </>
     );
   }
   return (null);
--- src/components/avatar/useStyles.js
+++ src/components/avatar/useStyles.js
@@ -1,9 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  root: {
+  avatar: {
     width: theme.spacing(4),
     height: theme.spacing(4),
     margin: '0.5em 0.5em 0.5em 0',
+    cursor: 'pointer',
   },
 }));
